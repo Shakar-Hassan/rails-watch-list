d11258da4cd1a37de2566f170baaab1cc21008ba
shakar hassan <shakarmohammed2002@gmail.com>
2023-09-21 12:02:42 +0200

    Upded routes to show index page and show

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  # get "movies", to: "movies#index"
-  get "lists", to: "lists#index"
-  get "lists/new", to: "lists#new", as: :new
-  post "lists", to: "lists#create"
-  get "lists/:id", to: "lists#show", as: :list
+  root "lists#index"
+  resources :list, only: %i[show]
 end
